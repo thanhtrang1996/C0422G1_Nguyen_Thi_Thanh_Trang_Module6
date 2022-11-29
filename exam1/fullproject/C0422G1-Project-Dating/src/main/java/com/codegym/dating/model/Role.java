a85de08f7825ec7a4b6bfe4d4c1db37e6e3ed6e7
Thanh Trang <you@example.com>
2022-11-29 17:00:47 +0700

    bt

---
+++ exam1/fullproject/C0422G1-Project-Dating/src/main/java/com/codegym/dating/model/Role.java
@@ -0,0 +1,23 @@
+package com.codegym.dating.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idRole;
+    private String roleName;
+    @OneToMany(mappedBy = "role")
+    private Set<AccountRole> accountRoles;
+}
